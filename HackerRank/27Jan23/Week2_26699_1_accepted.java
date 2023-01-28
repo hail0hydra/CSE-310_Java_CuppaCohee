51d7b0be2a99f09ae44a4349a44888bd7a4618e6
Chequita Corvette <thehokage0hiddenleaf@gmail.com>
2023-01-28 07:46:54 +0530

    These damn Test Cases

---
+++ HackerRank/27Jan23/Week2_26699_1_accepted.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Week2_26699_1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double rate = sc.nextDouble();
+
+        if (rate <= 5) {
+            System.out.println("thanks");
+        }
+
+        else {
+            System.out.println("101");
+        }
+    }
+}
